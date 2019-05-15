553f527a86f6ad602d72f4f3a7dfc9ace04d5600
Wang Shuhao <276254221@qq.com>
2019-05-16 00:41:50 +0800

    rabitmq

--- builtin/rabbitmq-proxy/rabbit.go
+++ builtin/rabbitmq-proxy/rabbit.go
@@ -1,6 +1,30 @@
 package main
 
-func main() {
+import (
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/youpenglai/apix/proxy"
+)
+
+func getRabbitMQConf() (addr, user, password, vhost string) {
+	return
 }
 
+func wait() (c chan os.Signal) {
+	c = make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGTERM)
+	return
+}
+
+func main() {
+	proxyInst := proxy.InitServiceProxy()
+	proxy.RegisterService(proxyInst, "")
+
+	proxy.HandleServiceCall(proxyInst, func(call *proxy.ProxyServiceCall) (ret []byte, err error) {
+		return
+	})
+
+	<-wait()
+}
